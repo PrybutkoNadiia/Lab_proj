e7b15efde8ed14be7b131a3216a1f10c98d73b53
Прибутько Надія <9781714@stud.kai.edu.ua>
2026-05-22 13:33:20 +0300

    Add files via upload

---
+++ HomeTask/MatrixContainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class MatrixContainer
+    {
+        private int[,] _matrix;
+
+        public MatrixContainer(int rows, int cols)
+        {
+            if (rows <= 0 || cols <= 0)
+            {
+                throw new ArgumentException("Розміри масиву повинні бути більшими за нуль.");
+            }
+            _matrix = new int[rows, cols];
+        }
+
+        public int Rows
+        {
+            get { return _matrix.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return _matrix.GetLength(1); }
+        }
+
+        public int this[int row, int col]
+        {
+            get
+            {
+                if (row >= 0 && row < Rows && col >= 0 && col < Columns)
+                {
+                    return _matrix[row, col];
+                }
+                else
+                {
+                    Console.WriteLine($"[Помилка індексатора]: Індекси [{row}, {col}] виходять за межі масиву (Розмір: {Rows}x{Columns}).");
+                    return 0;
+                }
+            }
+            set
+            {
+                if (row >= 0 && row < Rows && col >= 0 && col < Columns)
+                {
+                    _matrix[row, col] = value;
+                }
+                else
+                {
+                    Console.WriteLine($"[Помилка індексатора]: Спроба запису за межі масиву за індексами [{row}, {col}].");
+                }
+            }
+        }
+    }
+}
---
+++ HomeTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            MatrixContainer container = new MatrixContainer(3, 4);
+
+            Console.WriteLine($"Створено матрицю розміром: {container.Rows}x{container.Columns}");
+
+            int elementValue = 10;
+            for (int i = 0; i < container.Rows; i++)
+            {
+                for (int j = 0; j < container.Columns; j++)
+                {
+                    container[i, j] = elementValue;
+                    elementValue += 5;
+                }
+            }
+
+            Console.WriteLine("\nВміст матриці:");
+            for (int i = 0; i < container.Rows; i++)
+            {
+                for (int j = 0; j < container.Columns; j++)
+                {
+                    Console.Write($"{container[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n--- Тестування захисту індексатора від виходу за межі ---");
+
+            Console.WriteLine("Спроба зчитати container[5, 2]:");
+            int invalidRead1 = container[5, 2];
+
+            Console.WriteLine("\nСпроба зчитати container[1, -1]:");
+            int invalidRead2 = container[1, -1];
+
+            Console.WriteLine("\nСпроба записати значення 99 в container[2, 10]:");
+            container[2, 10] = 99;
+
+            Console.WriteLine("\nПрограма завершила роботу. Натисніть будь-яку клавішу...");
+            Console.ReadKey();
+        }
+    }
+}
